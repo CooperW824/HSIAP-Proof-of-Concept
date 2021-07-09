006bb954c4bbda7751e57a2294b5c8e21553d2b5
Cooper Werner <cooperwerner824@gmail.com>
2021-07-09 11:32:07 -0400

    comments and user selection for serial reader

--- Py_SRC/serialRead.py
+++ Py_SRC/serialRead.py
@@ -1,11 +1,16 @@
 import serial
-ser = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=10)  # open serial port
-line = ser.read(size=100)
-line = str(line)
-line = line[2:len(line)-1]
+ser = serial.Serial(input("Please enter the address to an active serial port: "),
+                    baudrate=int(input("Please enter a proper baud rate: ")),
+                    timeout=int(input("Please enter a valid timeout duration: ")))  # open serial port
+line = ser.read(size=100) # read upto 100 bytes
+line = str(line) #convert to string
+line = line[2:len(line)-1] #remove excess junk
 lineList = list(line)
-lineBin = ''.join(format(i, '08b') for i in bytearray(line, encoding ='utf-8'))
+lineBin = ''.join(format(i, '08b') for i in bytearray(line, encoding ='utf-8')) # convert to bits
+# outputs
 print(line)
 print(lineList)
 print(lineBin)
+
+# end the serial reader
 ser.close()
